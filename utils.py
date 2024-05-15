5e116bbe7170386a3817b14b283569c885341f2f
morfinix <morfinic@proton.me>
2024-05-15 14:02:10 +0200

    Dodanie brakujących docstringów oraz poprawiono formatowanie

--- utils.py
+++ utils.py
@@ -1,11 +1,44 @@
+"""
+Created on 15.05.2024
+This module has some basic utility functions
+"""
+
+
 def add(a: int, b: int) -> int:
+    """
+    Adds two integers
+    :param a: First integer
+    :param b: Second integer
+    :return: Sum of a and b
+    """
     return a + b
 
+
 def subtract(a: int, b: int) -> int:
+    """
+    Subtracts two integers
+    :param a: First integer
+    :param b: Second integer
+    :return: Subtracts b from a
+    """
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
+    """
+    Multiplies two integers
+    :param a: First integer
+    :param b: Second Integer
+    :return: Multiplies a by b
+    """
     return a * b
 
+
 def divide(a: int, b: int) -> float:
+    """
+    Divides two numbers
+    :param a: First integer
+    :param b: Second integer
+    :return: Divides a by b
+    """
     return a / b
